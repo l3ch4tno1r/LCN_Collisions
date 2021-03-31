555b5037fe816f8cf8e1dbbf8f81ff5299dfa49e
l3ch4tno1r <arnaud.granier34@gmail.com>
2021-03-31 23:39:45 +0200

    Minor commit

--- Source/Shapes/Plane.h
+++ Source/Shapes/Plane.h
@@ -13,12 +13,18 @@ namespace LCN
 		using HVectorType = HVector3Df;
 
 		Plane(const HVectorType& origin, const HVectorType& normal) :
-			Origin(origin),
-			Normal(normal)
+			m_Origin(origin),
+			m_Normal(normal)
 		{}
 
+		const HVectorType& Origin() const { return m_Origin; }
+		      HVectorType& Origin()       { return m_Origin; }
+
+		const HVectorType& Normal() const { return m_Normal; }
+		      HVectorType& Normal()       { return m_Normal; }
+
 	private:
-		HVectorType Origin;
-		HVectorType Normal;
+		HVectorType m_Origin;
+		HVectorType m_Normal;
 	};
 }
